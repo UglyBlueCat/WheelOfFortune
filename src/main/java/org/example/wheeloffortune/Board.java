69093aa795f0871b75b12026fbac096444f307d5
idreesrobinson <idrees.robinson@gmail.com>
2026-02-05 22:58:03 +0000

    Create anonymised phrase variable with test

--- src/main/java/org/example/wheeloffortune/Board.java
+++ src/main/java/org/example/wheeloffortune/Board.java
@@ -5,11 +5,21 @@ import java.util.List;
 public class Board {
     public List<String> players;
     private String phrase;
+    private char[] anonymisedPhrase;
 
     void setPhrase(String s){
         this.phrase = s;
     }
+
+    void setAnonymisedPhrase(char[] c){
+        this.anonymisedPhrase = c;
+    }
+
     String getPhrase(){
         return this.phrase;
+    }
+
+    public char[] getAnonymisedPhrase() {
+        return this.anonymisedPhrase;
     }
 }
--- src/test/java/org/example/wheeloffortune/BoardClassTests.java
+++ src/test/java/org/example/wheeloffortune/BoardClassTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootTest
 public class BoardClassTests {
@@ -28,5 +29,15 @@ public class BoardClassTests {
         var x = new Board();
         x.setPhrase("");
         Assert.state((x.getPhrase().isEmpty()), "Phrase variable exists");
+    }
+
+    @Test
+    void TestAnonymisedPhraseExists(){
+        var x = new Board();
+        char c = 'a';
+        x.setAnonymisedPhrase(new char[]{c});
+        System.out.print(c);
+        System.out.print(x.getAnonymisedPhrase());
+        Assert.state(Arrays.equals(x.getAnonymisedPhrase(), new char[]{c}),"Anonymised phrase variable exists");
     }
 }
