dccf92ca4673eb0aad27a7a1a64f4205901b171f
Robin Spinks <uglybluecat@googlemail.com>
2026-02-24 16:46:54 +0000

    Create test that Player variable called name exists

--- src/test/java/org/example/wheeloffortune/PlayerClassTests.java
+++ src/test/java/org/example/wheeloffortune/PlayerClassTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import java.lang.reflect.Field;
+
 @SpringBootTest
 public class PlayerClassTests {
 
@@ -15,6 +17,18 @@ public class PlayerClassTests {
             Assert.state(
                     false,
                     "Player Class does not exist"
+            );
+        }
+    }
+
+    @Test
+    void TestNameVariableExists() {
+        try {
+            Field field = Player.class.getDeclaredField("name");
+        } catch (NoSuchFieldException e) {
+            Assert.state(
+                    false,
+                    "Player variable name does not exist"
             );
         }
     }
