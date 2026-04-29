3f0f84cc43d273c094e06f3ae21efe1f2f46a098
Robin Spinks <uglybluecat@googlemail.com>
2026-04-29 16:41:26 +0100

    Add root endpoint so test passes

--- src/main/java/org/example/wheeloffortune/APIController.java
+++ src/main/java/org/example/wheeloffortune/APIController.java
@@ -1,7 +1,14 @@
 package org.example.wheeloffortune;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class APIController {
+
+    @GetMapping("/")
+    public String openGame() {
+        String response = "Default response from Wheel of Fortune";
+        return response;
+    }
 }
