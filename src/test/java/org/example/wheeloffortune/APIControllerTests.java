d23aed77c6745cfbe49e7d03ed59aa5a36e7411e
Robin Spinks <uglybluecat@googlemail.com>
2026-04-29 16:40:35 +0100

    Add failing test for default output from root API endpoint

--- src/test/java/org/example/wheeloffortune/APIControllerTests.java
+++ src/test/java/org/example/wheeloffortune/APIControllerTests.java
@@ -1,11 +1,28 @@
 package org.example.wheeloffortune;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.client.RestTestClient;
 import org.springframework.util.Assert;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
 public class APIControllerTests {
+
+    @Autowired
+    private APIController controller;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private RestTestClient restTestClient;
 
     @Test
     void ClassAPIControllerExists() {
@@ -17,5 +34,15 @@ public class APIControllerTests {
                     "APIController class does not exist"
             );
         }
+    }
+
+    @Test
+    void APIHasRootEndpoint() {
+        assertThat(controller).isNotNull();
+        restTestClient.get()
+                .uri("http://localhost:%d/".formatted(port))
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo("Default response from Wheel of Fortune");
     }
 }
