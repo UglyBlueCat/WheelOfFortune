77ad9fb3bd0426cf627ccc01bd4d737b5231f51b
Robin Spinks <uglybluecat@googlemail.com>
2026-05-08 16:25:20 +0100

    Add test confirming JSON returned by boardStatus() method contains an object named AnonymisedPhrase

--- src/test/java/org/example/wheeloffortune/BoardClassTests.java
+++ src/test/java/org/example/wheeloffortune/BoardClassTests.java
@@ -230,6 +230,21 @@ public class BoardClassTests {
     }
 
     @Test
+    void ConfirmBoardStatusMethodReturnsGuessedLetters() {
+        final Board board = new Board();
+        board.setPhrase("Test phrase");
+        final Set<Character> letters = Set.of('a', 'e');
+        board.setGuessedLetters(letters);
+        final String boardStatusJSON = board.boardStatus();
+        final JsonObject boardStatusJsonObject = JsonParser.parseString(boardStatusJSON).getAsJsonObject();
+
+        Assert.state(
+                boardStatusJsonObject.has("AnonymisedPhrase"),
+                "JSON string returned by Board.boardStatus() did not contain an object named AnonymisedPhrase"
+        );
+    }
+
+    @Test
     void CheckLetterMethodCorrectlyChecksLetters() {
         Board board = new Board();
         board.setPhrase("Test phrase");
