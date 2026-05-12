cd0ed2e30a30059adc8ee70dca87e3a40a8a01ac
Robin Spinks <uglybluecat@googlemail.com>
2026-05-12 17:26:24 +0100

    Add tests for getters & setters functioning correctly

--- src/test/java/org/example/wheeloffortune/PlayerClassTests.java
+++ src/test/java/org/example/wheeloffortune/PlayerClassTests.java
@@ -240,4 +240,26 @@ public class PlayerClassTests {
             Assert.state(false,"setCurrentPoints() method does not exist");
         }
     }
+
+    @Test
+    void variableGetSetNameGetsAndSets() {
+        final String sampleName = "Jimmy McJimmy";
+        final Player player = new Player();
+        player.setName(sampleName);
+        Assert.state(
+                sampleName.equals(player.getName()),
+                "The value set by Player.setName() was not the same as the value then returned by Player.getName()"
+        );
+    }
+
+    @Test
+    void variableGetSetCurrentPointsGetsAndSets() {
+        final Integer samplePoints = 3;
+        final Player player = new Player();
+        player.setCurrentPoints(samplePoints);
+        Assert.state(
+                samplePoints.equals(player.getCurrentPoints()),
+                "The value set by Player.setCurrentPoints() was not the same as the value then returned by Player.getCurrentPoints()"
+        );
+    }
 }
