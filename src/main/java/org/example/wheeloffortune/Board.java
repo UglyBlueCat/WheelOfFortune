610498eaf1834fc9856185d898ab3fb8122ded65
Robin Spinks <uglybluecat@googlemail.com>
2026-05-14 17:54:57 +0100

    Add javadoc

--- src/main/java/org/example/wheeloffortune/Board.java
+++ src/main/java/org/example/wheeloffortune/Board.java
@@ -92,6 +92,11 @@ public class Board {
         this.phrase = s;
     }
 
+    /**
+     * Ends the game
+     * <ul><li>Clears the board</li>
+     * <li>Resets guessed letters</li></ul>
+     */
     public void endGame() {
         this.setGuessedLetters(Set.of());
         this.setPhrase("");
