6edae1c6b9c532269df8078d6885a76deab3d689
Robin Spinks <uglybluecat@googlemail.com>
2026-05-28 16:35:02 +0100

    Add @Component annotation to Player class

--- src/main/java/org/example/wheeloffortune/Player.java
+++ src/main/java/org/example/wheeloffortune/Player.java
@@ -1,7 +1,9 @@
 package org.example.wheeloffortune;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Player {
     public String name;
     public Integer currentPoints;
